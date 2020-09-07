36ab9eb71abaf96ea1de2f891e9fbd283d82f553
Descifle <descifle_itb@yahoo.com>
2020-09-06 22:43:38 -0400

    switch back to production

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI
-// const uri = process.env.MONGODB_URI || 'mongodb://localhost/mytubemern';
+// const uri = process.env.ATLAS_URI
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/mytubemern';
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
 
 const connection = mongoose.connection;
