b6a2558e3778fd3981d7dc10163494f7d12d434b
Descifle <descifle_itb@yahoo.com>
2020-09-28 14:43:04 -0400

    confirmed??

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // change this to use production server as well
-app.use(cors({
-    origin: 'http://localhost:mytubemern',
-    credentials: true
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
