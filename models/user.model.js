eccb3492140d442de6335cb80578702ef0fc602c
Descifle <descifle_itb@yahoo.com>
2020-10-25 16:25:22 -0400

    removed unique from videos

--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {type: String,required: true,unique: true,trim: true,minlength: 2},
     password: {type: String,required: true,unique: true,trim: true,minlength: 3},
-    videos: {type: Array, required: true, unique: true, trim: true}
+    videos: {type: Array, required: true, unique: false, trim: true}
 }, {
     timestamps: true,
 }); 
