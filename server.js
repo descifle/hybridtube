c62d7a68050cd8f24e7f95598aa9ede7ddb406d8
Descifle <descifle_itb@yahoo.com>
2020-10-25 17:31:06 -0400

    default cors

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: `http://mytubemern.herokuapp.com:${port}`,
-    credentials: true
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
